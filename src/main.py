a52f7494bd9f23406b101e3b63569b6d400ee00d
Manuel Padron Martinez <manolopm@graph-ic.org>
2020-04-09 15:03:49 +0100

    Comment main.py

--- src/main.py
+++ src/main.py
@@ -2,10 +2,17 @@
 from scrapingLambo import scrapingLambo
 from porsche import scrapingPorsche
 import csv
+
+# Cabeceras para el fichero csv
 header = [['marca', 'modelo', 'altura', 'anchoBase', 'anchoTotal', 'longitud', 'peso',
            'cilindrada', 'consumo', 'potencia', 'aceleracion', 'velocidadMax', 'precio']]
+
+# Hace el scraping de Lamborghini
 lambo = scrapingLambo()
+
+# Hace el scraping de Porsche
 porsche = scrapingPorsche()
+
 with open('../csv/carsTecnicsFeatures.csv', 'w', newline='') as f:
     writer = csv.writer(f)
     writer.writerows(header)
